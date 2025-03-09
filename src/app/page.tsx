f94bf4949ab9c6efaf7fd12a7edc699dcd070363
ndsanja <ndsanja@gmail.com>
2025-03-09 13:08:08 +0700

    fix linting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-center mb-4 font-bold">
+      <h1 className="text-center mb-6 font-bold">
         Kampung Iggris Online Course
       </h1>
       <Button>
