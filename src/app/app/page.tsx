7544fdc77f4deb13249481effd4d5fb7a95d419d
user <user@users-MacBook-Pro.local>
2025-03-09 11:54:18 +0700

    initial commit

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -2,9 +2,9 @@ import { AppSidebar } from "@/app/app/dashboard/app-sidebar";
 import { SiteHeader } from "@/app/app/dashboard/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
-export const iframeHeight = "800px";
+// export const iframeHeight = "800px";
 
-export const description = "A sidebar with a header and a search form.";
+// export const description = "A sidebar with a header and a search form.";
 
 export default function Page() {
   return (
