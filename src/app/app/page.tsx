dcc73eb31f63feab44326ac8d10dc029c28e06ea
ndsanja <ndsanja@gmail.com>
2025-03-09 11:58:20 +0700

    fix initial commit

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -2,10 +2,6 @@ import { AppSidebar } from "@/app/app/dashboard/app-sidebar";
 import { SiteHeader } from "@/app/app/dashboard/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
-// export const iframeHeight = "800px";
-
-// export const description = "A sidebar with a header and a search form.";
-
 export default function Page() {
   return (
     <div className="[--header-height:calc(--spacing(14))]">
