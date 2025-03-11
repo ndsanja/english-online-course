8187bbe628c35c1aebb8d2663ead08906fb6947f
ndsanja <ndsanja@gmail.com>
2025-03-11 19:36:28 +0700

    membuat fungsi tambah markdown

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Ini akan disable ESLint selama build dan dev
+  ignoreDuringBuilds: true,
 };
 
 export default nextConfig;
